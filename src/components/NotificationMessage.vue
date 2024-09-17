7d6f922ad35460247163f507e47139c1e8240496
Elvira Morán <elvira@frescocooks.com>
2024-09-17 10:05:02 +0200

    feat: add notifcations component

---
+++ src/components/NotificationMessage.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'info'
+    },
+    header: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    onClose() {
+      this.visible = false
+    }
+  }
+}
+</script>
+<template>
+  <div class="ui message" :class="type" v-if="visible">
+    <i class="close icon" @click="onClose"></i>
+    <div class="header">{{ header }}</div>
+    <slot></slot>
+  </div>
+</template>
---
+++ src/views/NotificationsView.vue
@@ -0,0 +1,33 @@
+<script>
+import NotificationMessage from '@/components/NotificationMessage.vue'
+
+export default {
+  components: {
+    NotificationMessage
+  }
+}
+</script>
+<template>
+  <div id="app" class="ui container">
+    <h1>Notification Center</h1>
+
+    <NotificationMessage type="info" header="Notification 1"
+      ><div>This is the content of the first notification</div></NotificationMessage
+    >
+    <NotificationMessage type="warning" header="Notification 2"
+      ><div>This is the content of the second notification</div></NotificationMessage
+    >
+    <NotificationMessage type="error" header="Notification 3"
+      ><div>This is the content of the third notification</div></NotificationMessage
+    >
+    <NotificationMessage type="success" header="Notification 4"
+      ><div>This is the content of the fourth notification</div></NotificationMessage
+    >
+  </div>
+</template>
+<style scoped>
+h1 {
+  text-align: center;
+  padding: 20px
+}
+</style>
