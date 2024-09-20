d65323345ba42e5e0ac1bd6c4dd98dad61849cda
Elvira Morán <elvira@frescocooks.com>
2024-09-20 15:23:45 +0200

    feat: project scaffolding and basic todo list

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,45 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue'
+
+interface Item {
+  id: string
+  label: string
+  isHighPriority: boolean
+}
+
+const header = ref('Shopping list APP')
+const items = ref<Item[]>([])
+const newItem = ref('')
+const isNewItemHighPriority = ref(false)
+
+const addItem = () => {
+  items.value.push({
+    id: (items.value.length + 1).toString(),
+    label: newItem.value,
+    isHighPriority: isNewItemHighPriority.value
+  })
+  newItem.value = ''
+  isNewItemHighPriority.value = false
+}
+
+const characterCount = computed(() => newItem.value.length)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>{{ header }}</h1>
+  <input type="text" placeholder="Add an item" v-model="newItem" v-on:keyup.enter="addItem" />
+  <span>{{ characterCount }} characters</span>
+  <label>
+    <input type="checkbox" v-model="isNewItemHighPriority" />
+    High priority
+  </label>
+  <input type="button" value="Add" v-on:click="addItem" class="btn bnt-primary" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div v-if="!items.length">There are no items. Start adding one</div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div v-for="item in items" :key="item.id">
+    {{ item.label }}
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
